8d05fc7adc055008752fe720136efef7f9d7c592
manavagr1108 <manavagr1108@gmail.com>
2023-02-11 04:11:04 +0530

    - [fix] connection btw background and devtool - [feat] get the sum of transfered data

--- apps/extension/devtools.js
+++ apps/extension/devtools.js
@@ -1,20 +1,27 @@
 var backgroundPageConnection = chrome.runtime.connect({
-    name: "devtools_page"
+  name: "devtools_page",
 });
 
-backgroundPageConnection.onMessage.addListener(function (message) {
-    // Handle responses from the background page, if any
-    const data = chrome.devtools.network.getHAR();
+backgroundPageConnection.onMessage.addListener(async function (message) {
+  // Handle responses from the background page, if any
+  // const data = await chrome.devtools.network;
+  let network = await chrome.devtools.network;
+  let data;
+  await chrome.devtools.network.onRequestFinished.addListener(function (req) {
+    data = req;
+
+    // window.alert(Object.keys(req));
     backgroundPageConnection.postMessage({
         tabId: message.tabId,
-        data: data
-    })
+        data:data
+    });
+  });
 });
 
 // Relay the tab ID to the background page
 backgroundPageConnection.postMessage({
-    tabId: chrome.devtools.inspectedWindow.tabId,
-    scriptToInject: "./content_scripts/content.js"
+  tabId: chrome.devtools.inspectedWindow.tabId,
+  scriptToInject: "./content_scripts/content.js",
 });
 
 // console.log("devtools")
--- apps/extension/index.js
+++ apps/extension/index.js
@@ -1,15 +1,16 @@
+let sum = 0;
 chrome.runtime.onConnect.addListener(function (devToolsConnection) {
   // assign the listener function to a variable so we can remove it later
   console.log("conneted");
   var devToolsListener = function (message, sender, sendResponse) {
     // Inject a content script into the identified tab
     console.log(message);
+    if(message.data){
+        sum+=message.data.request.bodySize + message.data.response.content.size
+        console.log(sum);
+    }
     if(message.scriptToInject !== undefined){
-        // chrome.scripting.executeScript({
-        //     target : {tabId : message.tabId},
-        //     files : [ message.scriptToInject ],
-        //   });
-          sendResponse({tabId:message.tabId})
+        devToolsConnection.postMessage({'tabId':message.tabId})
     }
   };
   // add the listener
