53903c382db5fd990fbc5abebd64cd733704dfec
Raghav Khullar <khullarraghav2001@gmail.com>
2023-02-11 07:10:05 +0530

    fix(server):Bug Fixes

--- apps/extension/index.js
+++ apps/extension/index.js
@@ -4,20 +4,20 @@ chrome.runtime.onConnect.addListener(function (devToolsConnection) {
   var devToolsListener = function (message, sender, sendResponse) {
     // Inject a content script into the identified tab
     sum.find(d => {
-        if(d.tabId === message.tabId){
-            d.sum += message.data.request.bodySize + message.data.response.content.size
-            const reg = message.data._initiator.url.match(/http?s:\/\/[A-Za-z0-9.]*/);
-            d.url = reg;
-            console.log(d);
-        }
+      if (d.tabId === message.tabId) {
+        d.sum += message.data.request.bodySize + message.data.response.content.size
+        const reg = message.data._initiator.url.match(/http?s:\/\/[A-Za-z0-9.]*/);
+        d.url = reg;
+        console.log(d);
+      }
     })
-    if(message.scriptToInject !== undefined){
-        devToolsConnection.postMessage({'tabId':message.tabId})
-        sum.push({
-            tabId: message.tabId,
-            url: "",
-            sum: 0
-        })
+    if (message.scriptToInject !== undefined) {
+      devToolsConnection.postMessage({ 'tabId': message.tabId })
+      sum.push({
+        tabId: message.tabId,
+        url: "",
+        sum: 0
+      })
     }
   };
   // add the listener
@@ -27,3 +27,18 @@ chrome.runtime.onConnect.addListener(function (devToolsConnection) {
     devToolsConnection.onMessage.removeListener(devToolsListener);
   });
 });
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  fetch(
+    'http://localhost:4000/storeData', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      tabId: tabId,
+      sum: sum,
+    })
+  }
+  ).then((data) => console.log(data)).catch((err) => console.log(err));
+})
